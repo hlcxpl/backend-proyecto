b6f84442306c9fff53b18ca4d1ea96aa44eff90f
Luis O <30819865+hlcxpl@users.noreply.github.com>
2023-04-08 01:22:51 -0400

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT= process.env.PORT
 app.listen(PORT || 3000, () => console.log("SERVER ON IN PORT:",PORT))
 
 const corsOptions = {
-  origin: `${process.env.API_URL}/`,
+  origin: `*`,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
